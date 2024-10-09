7c41c12be753a798cc31336511c678828b8d1502
Martin Brandl <mjibrandl@gmail.com>
2024-10-09 10:53:45 +0200

    finalize style

--- src/python/ki-weisswurstfruehstueck-kufstein/chat.py
+++ src/python/ki-weisswurstfruehstueck-kufstein/chat.py
@@ -1,4 +1,5 @@
 import asyncio
+import os
 
 from semantic_kernel.agents import AgentGroupChat
 from semantic_kernel.agents.strategies.termination.termination_strategy import TerminationStrategy
@@ -17,7 +18,7 @@ async def main():
     agent_social = SocialCraftAgent.create()
 
     chat = AgentGroupChat(
-        agents=[agent_seo, agent_social],
+        agents=[agent_social, agent_seo],
         termination_strategy=ApprovalTerminationStrategy(agents=[agent_seo], maximum_iterations=15),
     )
 
@@ -28,11 +29,11 @@ async def main():
     await chat.add_chat_message(ChatMessageContent(role=AuthorRole.USER, content=input))
 
     async for content in chat.invoke():
-        print(f"\033[91m{content.role} - {content.name or '*'}\033[0m: '{content.content}'")
-        print('_' * 10) 
+        print(f"\033[91m{content.name}:\033[0m")
+        print(content.content)
+        print('_' * os.get_terminal_size().columns) 
 
     print(f"# IS COMPLETE: {chat.is_complete}")
-
 
 if __name__ == "__main__":
     asyncio.run(main())
--- src/python/ki-weisswurstfruehstueck-kufstein/seo_boost_agent.py
+++ src/python/ki-weisswurstfruehstueck-kufstein/seo_boost_agent.py
@@ -6,7 +6,7 @@ AGENT_NAME = "SeoBoostAgent"
 AGENT_INSTRUCTIONS = """
 You are an SEO optimizer with deep expertise in search engine algorithms and a focus on driving organic traffic. 
 Your goal is to evaluate whether the provided content is optimized for SEO best practices. 
-If the content meets standards, confirm that it is approved. 
+If the content meets standards, confirm that it is approved. ALWAYS use the word "approved" in that case.
 If not, provide insights on how to improve SEO factors, without giving specific examples in a SINGLE proposal per respond. Focus on one point at a time. Keep it short!.
 """
 
